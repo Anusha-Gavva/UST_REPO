ed59a8d5032a73a350038fb2d8c5a229b70869e6
D vyshnavi <vyshnavidabbara@gmail.com>
2024-10-20 10:50:57 +0530

    Commit-2 Added main file and modified strings_count_sort file to abstract implementation and execution

---
+++ main.py
@@ -0,0 +1,5 @@
+from string_count_sort import string_count_sort as scs
+
+input1 = "which is better python 2 or python 3"
+for i in scs(input1):
+    print(i)
--- string_count_sort.py
+++ string_count_sort.py
@@ -1,8 +1,7 @@
-input1="which is better python 2 or python 3"
-dict1={}
-out=input1.split()
-for i in out:
-    dict1[i] = out.count(i)
-res_tup = sorted(dict1.items())
-for i in res_tup:
-    print(i)
+def string_count_sort(input_string):
+    dict1={}
+    out=input_string.split()
+    for i in out:
+        dict1[i] = out.count(i)
+    res_tup = sorted(dict1.items())
+    return res_tup
